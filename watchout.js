2c587284baf9adf2cc014b2e4cbf8ba77edfbcd6
zhengcano <zhengcano@gmail.com>
2015-02-13 15:40:25 -0800

    Add drag functionality to player

--- watchout.js
+++ watchout.js
@@ -15,11 +15,19 @@ enemies.each(function(enemy) {
   .attr('cy', Math.floor(Math.random() * 560) + 20);
 });
 
+var drag = d3.behavior.drag().on("drag", function(d){
+  console.log(d3.event);
+  var x = d3.event.x;
+  var y = d3.event.y;
+  d3.select(this).attr("transform", 'translate('+ x +','+ y +')');
+})
+
 var player = d3.select('svg').selectAll('player')
   .data(['player']).enter().append('circle')
-  .attr('r', 20).attr('cx', 300).attr('cy', 300)
+  .attr('r', 20).attr('x', 0).attr('y', 0)
   .attr('fill', 'transparent').attr('stroke-width', 5)
-  .attr('stroke', 'green');
+  .attr('stroke', 'green').call(drag);
+
 
 
 setInterval(function(){
